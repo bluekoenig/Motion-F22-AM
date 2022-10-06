86bf0b7d543bd6e525e000cb0a64df48e04b95c8
Blue <bk647@drexel.edu>
2022-10-06 10:44:30 -0400

    wow she works

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -10,8 +10,27 @@ tl.from("#first-line",{duration:0.5, alpha:0})
     return tl;
     }
     
+
+    var buttonAnimation = gsap.timeline({paused:true});
+
+buttonAnimation.to("#trail-btn",{duration:0.25, scale:2})
+.to("#trail-btn i",{duration:0.25, rotateY: 180})
+.to("#first-line",{duration:0.25, alpha:0, y:50})
+.to("#second-line",{duration:0.25, alpha:0, y:25});
+
+
+// gsap.to("#trail-btn",{duration:0.25, scale:1});
+// gsap.to("#trail-btn i",{duration:0.25, rotateY: 0});
+
+// gsap.to("#first-line",{duration:0.25, alpha:1, y:0});
+// gsap.to("#second-line",{duration:0.25, alpha:1, y:0});
+
+
+
     var mainTL = gsap.timeline();
 mainTL.add(heroAnimation);
+
+
 
 // gsap.set("button i",{transformOrigin:"center bottom"});
 
@@ -20,20 +39,12 @@ mainTL.add(heroAnimation);
 // gsap.from("#trail-btn",{duration:0.5, alpha:0, y:50, delay:0.35});
 // gsap.from("#trail-btn i",{duration:0.25, alpha:0, rotation:90, delay:0.45});
 
-// let button = document.querySelector("#trail-btn");
-// button.addEventListener("mouseover",function(){
-//     gsap.to("#trail-btn",{duration:0.25, scale:2});
-//     gsap.to("#trail-btn i",{duration:0.25, rotateY: 180});
+let button = document.querySelector("#trail-btn");
+button.addEventListener("mouseover",function(){
+    buttonAnimation.play();
 
-//     gsap.to("#first-line",{duration:0.25, alpha:0, y:50});
-//     gsap.to("#second-line",{duration:0.25, alpha:0, y:25});
+});
 
-// });
-
-// button.addEventListener("mouseout",function(){
-//     gsap.to("#trail-btn",{duration:0.25, scale:1});
-//     gsap.to("#trail-btn i",{duration:0.25, rotateY: 0});
-
-//     gsap.to("#first-line",{duration:0.25, alpha:1, y:0});
-//     gsap.to("#second-line",{duration:0.25, alpha:1, y:0});
-// });
+button.addEventListener("mouseout",function(){
+    buttonAnimation.reverse();
+});
